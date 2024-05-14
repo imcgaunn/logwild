b6a257b4f9ec8cb5934d5de323b4f16603bcf268
ian mcgaunn <ian@mcgaunn.com>
2024-05-14 10:23:45 -0400

    update run_everything_in_panels script to work better

--- scripts/run_everything_in_panels.sh
+++ scripts/run_everything_in_panels.sh
@@ -5,32 +5,40 @@ setopt no_unset
 setopt pipe_fail
 
 SCRIPT_DIR="${0:A:h}"
+SESS_NAME="logwild_demo"
 
 function die() {
-  echo "did i die?"
+  echo "i am dead"
+  exit 222
 }
 
 function cleanup() {
-	printf "killing session created\n"
-	tmux kill-session -t observe_demo
-	printf "successfully cleaned up\n"
+  for window_addr in "1.1" "1.2" "1.3"; do
+    printf "sending ctrlc to window_addr: %s\n" "${window_addr}"
+    tmux send-keys -t ${SESS_NAME}:${window_addr} C-c
+  done
+  printf "successfully cleaned up\n"
 }
 
-# create a new tmux session
-tmux new-session -d -s observe_demo
-tmux send-keys -t observe_demo:1.1 'just run-observe-backend' C-m
-# split window horizontally
-tmux split-window -t observe_demo:1.1 -h
-tmux send-keys -t observe_demo:1.2 'just run-otel-collector' C-m
-# split window vertically
-tmux split-window -t observe_demo:1.1 -v
-tmux send-keys -t observe_demo:1.3 'just run-app' C-m
-if [ -z "$TMUX" ]; then
-  echo "tmux not set"
-  tmux attach-session -t observe_demo
-else
-  echo "tmux set"
-  tmux switchc -t observe_demo
+trap 'cleanup' EXIT
+
+if tmux has-session -t "${SESS_NAME}" 2>/dev/null; then
+
+  printf "killing existing session %s\n" "${SESS_NAME}"
+  tmux kill-session -t "${SESS_NAME}"
 fi
 
-echo "do we know at this point that we have exited the new tmux sess?"
+# create a new tmux session
+tmux new-session -d -s ${SESS_NAME}
+# run openobserve backend in first pane in first window
+tmux send-keys -t ${SESS_NAME}:1.1 'just run-observe-backend' C-m
+# split first pane horizontally
+tmux split-window -t ${SESS_NAME}:1.1 -h
+# start otelcol in new pane we just created
+tmux send-keys -t ${SESS_NAME}:1.2 'just run-otel-collector' C-m
+# split the new pane vertically
+tmux split-window -t ${SESS_NAME}:1.2 -v
+# run logwild server in the new vertical split
+tmux send-keys -t ${SESS_NAME}:1.3 'just run-app' C-m
+
+read "REPLY?program running in other window waiting"
