859a13bd8db1cbcb1ca9b4699fefe3ffd18aaf66
ian mcgaunn <ian@mcgaunn.com>
2024-05-20 17:48:46 -0400

    basically works - but weird

--- pkg/api/http/loggen.go
+++ pkg/api/http/loggen.go
@@ -19,14 +19,29 @@ import (
 // @Success 200 {object} api.LogStatsResponse
 // @Router /api/loggen [get]
 func (s *Server) logGenHandler(w http.ResponseWriter, r *http.Request) {
-	queryVals := r.URL.Query()
 	_, span := s.tracer.Start(r.Context(), "logGenHandler")
 	defer span.End()
+	h := s.createLogHandlerOrPanic()
+	// create initial options from config
+	optFuncs := s.buildLoggerOptionsFromConfig()
+	// override functions based on query params
+	optFuncs = append(optFuncs, s.buildLoggerOptionsFromQueryParams(h, r)...)
+	lm := logmaker.NewLogMaker(optFuncs...)
+	slog.Info("lm config", "perSecondRate", lm.PerSecondRate)
+	donech := make(chan int)
+	go func() {
+		err := lm.StartWriting(donech)
+		if err != nil {
+			panic(err) // something bad here
+		}
+	}()
+	logCount := <-donech
+	data := LogStatsResponse{logCount: logCount}
+	s.JSONResponse(w, r, data)
+}
+
+func (s *Server) createLogHandlerOrPanic() *slog.JSONHandler {
 	lvl := &slog.LevelVar{}
-	s.logger.Info("config params", "perSecondRate", s.config.LogwildPerSecondRate, "outFile", s.config.LogwildOutFile)
-	perSecondRateParam := queryVals.Get("per_second")
-	perMessageSizeParam := queryVals.Get("message_size")
-	burstDuration := queryVals.Get("burst_dur")
 
 	var fp *os.File
 	if s.config.LogwildOutFile == "-" {
@@ -40,11 +55,29 @@ func (s *Server) logGenHandler(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 	h := slog.NewJSONHandler(fp, &slog.HandlerOptions{Level: lvl})
-	// depepending on values passed in through query string, construct
-	// a new log maker with the appropriate options.
+	return h
+}
+
+func (s *Server) buildLoggerOptionsFromConfig() []logmaker.OptFunc {
 	var optFuncs []logmaker.OptFunc
+	h := s.createLogHandlerOrPanic()
+	optFuncs = append(optFuncs, logmaker.WithLogger(slog.New(h)))
+	optFuncs = append(optFuncs, logmaker.WithPerSecondRate(s.config.LogwildPerSecondRate))
+	return optFuncs
+}
+
+func (s *Server) buildLoggerOptionsFromQueryParams(h *slog.JSONHandler, r *http.Request) []logmaker.OptFunc {
+	var optFuncs []logmaker.OptFunc
+	queryVals := r.URL.Query()
+	_, span := s.tracer.Start(r.Context(), "handleQueryParams")
+	defer span.End()
+	perSecondRateParam := queryVals.Get("per_second")
+	perMessageSizeParam := queryVals.Get("message_size")
+	burstDuration := queryVals.Get("burst_dur")
+	s.logger.Info("config params", "perSecondRate", s.config.LogwildPerSecondRate, "outFile", s.config.LogwildOutFile)
+
 	if perSecondRateParam != "" {
-		slog.Debug("handling perSecondRateParam", "perSecondRateParam", perSecondRateParam)
+		s.logger.Debug("handling perSecondRateParam", "perSecondRateParam", perSecondRateParam)
 		perSecondRateInt, err := strconv.ParseInt(perSecondRateParam, 10, 64)
 		if err != nil {
 			panic(err) // could not convert perSecondRate param to integer :(
@@ -52,7 +85,7 @@ func (s *Server) logGenHandler(w http.ResponseWriter, r *http.Request) {
 		optFuncs = append(optFuncs, logmaker.WithPerSecondRate(perSecondRateInt))
 	}
 	if perMessageSizeParam != "" {
-		slog.Debug("handling perMessageSizeParam", "perMessageSizeParam", perMessageSizeParam)
+		s.logger.Debug("handling perMessageSizeParam", "perMessageSizeParam", perMessageSizeParam)
 		perMessageSizeInt, err := strconv.ParseInt(perMessageSizeParam, 10, 64)
 		if err != nil {
 			panic(err) // could not convert perMessageSize param to integer :(
@@ -60,7 +93,7 @@ func (s *Server) logGenHandler(w http.ResponseWriter, r *http.Request) {
 		optFuncs = append(optFuncs, logmaker.WithPerMessageSizeBytes(perMessageSizeInt))
 	}
 	if burstDuration != "" {
-		slog.Debug("handling burstDuration", "burstDuration", burstDuration)
+		s.logger.Debug("handling burstDuration", "burstDuration", burstDuration)
 		burstDurationInt, err := strconv.ParseInt(burstDuration, 10, 0)
 		if err != nil {
 			panic(err)
@@ -68,18 +101,8 @@ func (s *Server) logGenHandler(w http.ResponseWriter, r *http.Request) {
 		optFuncs = append(optFuncs, logmaker.WithBurstDuration(time.Second*time.Duration(burstDurationInt)))
 	}
 	optFuncs = append(optFuncs, logmaker.WithLogger(slog.New(h)))
-	lm := logmaker.NewLogMaker(optFuncs...)
-	slog.Info("lm config", "perSecondRate", lm.PerSecondRate)
-	donech := make(chan int)
-	go func() {
-		err := lm.StartWriting(donech)
-		if err != nil {
-			panic(err) // something bad here
-		}
-	}()
-	logCount := <-donech
-	data := LogStatsResponse{logCount: logCount}
-	s.JSONResponse(w, r, data)
+	s.logger.Info("configured optFuncs", "optFuncs", optFuncs)
+	return optFuncs
 }
 
 type LogStatsResponse struct {
