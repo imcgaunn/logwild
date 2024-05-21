43f8ad88f9e699875062f4c1b1f8deec140ad5f2
ian mcgaunn <ian@mcgaunn.com>
2024-05-20 21:09:37 -0400

    reduce number of words in fake sentences to avoid taxing rng

--- pkg/logmaker/fakedata.go
+++ pkg/logmaker/fakedata.go
@@ -5,5 +5,5 @@ import "github.com/brianvoe/gofakeit/v7"
 func GetFakeSentence() string {
 	// if you try to generate too many random words
 	// leads to bottlenecks
-	return gofakeit.Sentence(150)
+	return gofakeit.Sentence(64)
 }
