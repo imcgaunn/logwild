343d0c9f3ecce221300edba43ec40b0033435bd4
ian mcgaunn <ian@mcgaunn.com>
2025-08-06 19:28:55 -0400

    i don't understand why tests fail in github actions, but making a safe tweak

--- pkg/logmaker/logmaker_test.go
+++ pkg/logmaker/logmaker_test.go
@@ -77,9 +77,9 @@ func TestThatLogMakerAppendsToExistingFile(t *testing.T) {
 	donech := make(chan int)
 	hdl := slog.NewTextHandler(f, &slog.HandlerOptions{Level: slog.LevelDebug})
 	mkr := NewLogMaker(WithLogger(slog.New(hdl)),
-		WithPerSecondRate(5000),
-		WithPerMessageSize(1024),
-		WithBurstDuration(5*time.Second))
+		WithPerSecondRate(100),
+		WithPerMessageSize(5),
+		WithBurstDuration(1*time.Second))
 	go func() {
 		err := mkr.StartWriting(donech)
 		if err != nil {
