926516901d1e3eb4fd1ffe25bd905a636f211b6c
ian mcgaunn <ian@mcgaunn.com>
2025-08-06 18:14:36 -0400

    update construction tests with new default value

--- pkg/logmaker/logmaker_test.go
+++ pkg/logmaker/logmaker_test.go
@@ -3,6 +3,7 @@ package logmaker
 import (
 	"log/slog"
 	"os"
+	"strings"
 	"testing"
 	"time"
 )
@@ -17,7 +18,7 @@ func TestCanMakeLogMaker(t *testing.T) {
 	if mkr.PerSecondRate != 1000 {
 		t.FailNow()
 	}
-	if mkr.PerMessageSize != 2*1024 {
+	if mkr.PerMessageSize != 48 {
 		t.FailNow()
 	}
 }
@@ -50,4 +51,58 @@ func TestThatLogMakerLogsToConfiguredLogger(t *testing.T) {
 	}()
 	wroteMessages := <-donech
 	t.Logf("finished test and wrote %d\n", wroteMessages)
+}
+
+func TestThatLogMakerAppendsToExistingFile(t *testing.T) {
+	// get a temporary file and setup logger to write to it
+	f, err := os.CreateTemp("", "logmakrtest")
+	if err != nil {
+		t.Errorf("something bad happened trying to open temp file %s\n", err)
+	}
+	// if we did open the file, clean it up at the end of the test
+	defer func(name string) {
+		err := os.Remove(name)
+		if err != nil {
+			panic(err) // really too late to do anything at this point :/
+		}
+	}(f.Name())
+
+	// write some initial content to the file
+	initialContent := "initial content"
+	_, err = f.WriteString(initialContent)
+	if err != nil {
+		t.Errorf("something bad happened trying to write to temp file %s\n", err)
+	}
+
+	donech := make(chan int)
+	hdl := slog.NewTextHandler(f, &slog.HandlerOptions{Level: slog.LevelDebug})
+	mkr := NewLogMaker(WithLogger(slog.New(hdl)),
+		WithPerSecondRate(5000),
+		WithPerMessageSize(1024),
+		WithBurstDuration(5*time.Second))
+	go func() {
+		err := mkr.StartWriting(donech)
+		if err != nil {
+			panic(err)
+		}
+	}()
+	wroteMessages := <-donech
+	t.Logf("finished test and wrote %d log messages\n", wroteMessages)
+
+	// read the file and verify that the original content is still present
+	// and that the new logs have been appended to the end of the file
+	_, err = f.Seek(0, 0)
+	if err != nil {
+		t.Errorf("something bad happened trying to seek to the beginning of the temp file %s\n", err)
+	}
+	fileContent, err := os.ReadFile(f.Name())
+	if err != nil {
+		t.Errorf("something bad happened trying to read the temp file %s\n", err)
+	}
+	if !strings.HasPrefix(string(fileContent), initialContent) {
+		t.Errorf("expected file to start with '%s', but it didn't", initialContent)
+	}
+	if len(string(fileContent)) <= len(initialContent) {
+		t.Errorf("expected file to contain more than just the initial content, but it didn't")
+	}
 }
